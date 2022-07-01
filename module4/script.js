38fc1d052aa571ee2c3bdf0f8ef70c9b35b4a65e
Ananya Bhat <71872745+ananyabhat-18@users.noreply.github.com>
2022-07-01 21:49:02 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,15 +1,15 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
+for (var i = 0; i < name.length; i++) {
+  var firstLetter = name[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
+    byeSpeaker(name[i]);
   }
   else {
-    helloSpeaker(names[i]);
+    helloSpeaker(name[i]);
   }
 }
 
