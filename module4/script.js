c286d97a9e47f651eee90a5b01e7165d6b3196b4
Ananya Bhat <71872745+ananyabhat-18@users.noreply.github.com>
2022-07-01 21:54:33 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -3,9 +3,9 @@
 var name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < name.length; i++) {
-  var firstLetter = name[i].charAt(0).toLowerCase();
+  var startLetter = name[i].charAt(0).toLowerCase();
 
-  if (firstLetter === 'j') {
+  if (startLetter === 'j') {
     byeSpeaker(name[i]);
   }
   else {
